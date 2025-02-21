be244f37b98f33e25eab7bac79b198910ad59c05
Nikita-Saly <saliy414@yandex.ru>
2025-02-21 19:29:57 +0300

    добавление для модально окна, выход при клике вне его области

--- scripts.js
+++ scripts.js
@@ -18,6 +18,21 @@ function closeModalOutside(modalId, event) {
   }
 }
 
+const modal = document.getElementById('modal1');
+const menuLinks = document.querySelectorAll('a');
+
+window.addEventListener('click', (event) => {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+});
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+});
+
 // Назначаем обработчики событий для кнопок закрытия
 const closeButtons = document.querySelectorAll('.close');
 closeButtons.forEach(button => {
