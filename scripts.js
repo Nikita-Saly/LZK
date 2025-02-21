b0f716bc62f80c4cd0831b40d3b803db203aec82
Nikita-Saly <saliy414@yandex.ru>
2025-02-21 15:53:02 +0300

    добавление модального окна и его стилей

--- scripts.js
+++ scripts.js
@@ -1,31 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var triggers = document.querySelectorAll('.modal-trigger');
 
-  triggers.forEach(function(trigger) {
-    trigger.addEventListener('click', function(event) {
-      event.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
 
-      // Определяем уникальный идентификатор текущего блока
-      var currentBannerId = trigger.closest('section').id;
+   
+// Функция для открытия модального окна
+function openModal(modalId) {
+  document.getElementById(modalId).style.display = "block";
+}
 
-      // Получаем родительский div секции
-      var parentSection = document.getElementById(currentBannerId);
+// Функция для закрытия модального окна
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = "none";
+}
 
-      // Получаем элементы image и text-info для текущего блока
-      var imageBlock = parentSection.querySelector(`[data-id="image-${currentBannerId}"]`);
-      var textInfoBlock = parentSection.querySelector(`[data-id="text-info-${currentBannerId}"]`);
+function closeModalOutside(modalId, event) {
+  if (event.target.classList.contains('modal')) {
+    closeModal(modalId);
+  }
+}
 
-      // Проверяем существование элементов
-      if (!imageBlock || !textInfoBlock) {
-        console.error("Один из нужных элементов не найден!");
-        return;
-      }
-
-      // Переключаем классы для смены отображаемых элементов
-      imageBlock.classList.toggle('hide');
-      imageBlock.classList.toggle('show');
-      textInfoBlock.classList.toggle('show');
-      textInfoBlock.classList.toggle('hide');
-    });
+// Назначаем обработчики событий для кнопок закрытия
+const closeButtons = document.querySelectorAll('.close');
+closeButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modalId = button.parentElement.parentElement.id;
+    closeModal(modalId);
   });
+});
+
+// Обработчик клика по ссылке
+const modalTriggers = document.querySelectorAll('.modal-trigger');
+modalTriggers.forEach(trigger => {
+  trigger.addEventListener('click', (event) => {
+    event.preventDefault(); // Предотвращаем переход по ссылке
+    const modalId = trigger.dataset.modal;
+    openModal(modalId);
+  });
+});
 });
